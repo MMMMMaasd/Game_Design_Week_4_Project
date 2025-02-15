afe7cc80dc52d95d84739b122d2252bee2d48d95
MMMMMaasd <zb2253@nyu.edu>
2025-02-15 14:24:26 +0800

    Small updates

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    public float speed;
+    private Rigidbody2D body;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
-        
+        body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update() {
-        
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        body.linearVelocity = new Vector2(horizontalInput * speed, body.linearVelocity.y);
     }
 }

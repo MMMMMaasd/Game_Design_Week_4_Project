d917144bd68bad4e132ab49e442221362c678a79
MMMMMaasd <zb2253@nyu.edu>
2025-02-16 05:35:09 +0800

    Remove Folder

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -22,11 +22,11 @@ public class PlayerController : MonoBehaviour
         body.linearVelocity = new Vector2(horizontalInput_with_speed, body.linearVelocity.y);
         if (horizontalInput < 0)
         {
-            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
         }
         else if (horizontalInput > 0)
         {
-            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
         }
     }
 }

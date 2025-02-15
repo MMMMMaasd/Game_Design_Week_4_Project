049c4c7d6e6959d26c85bd64edc8d20e7688f7c3
MMMMMaasd <zb2253@nyu.edu>
2025-02-15 16:40:03 +0800

    Updates

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public float FollowSpeed = 2f;
+    public float YOffset = 1f;
     public Transform target;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,7 +14,7 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x, target.position.y, -10.0f);
+        Vector3 newPos = new Vector3(target.position.x, target.position.y + YOffset, -10.0f);
         transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,32 @@
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
     public float speed;
+    public Animator animator;
     private Rigidbody2D body;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start() {
+    void Start()
+    {
         body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float horizontalInput_with_speed = horizontalInput * speed;
 
-        body.linearVelocity = new Vector2(horizontalInput * speed, body.linearVelocity.y);
+        animator.SetFloat("Speed", Mathf.Abs(horizontalInput_with_speed));
+
+        body.linearVelocity = new Vector2(horizontalInput_with_speed, body.linearVelocity.y);
+        if (horizontalInput < 0)
+        {
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+        else if (horizontalInput > 0)
+        {
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
     }
 }

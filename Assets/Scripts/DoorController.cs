2f0d37a98bccebe7c5295f251eb586785c0b8dc5
Robotgiggle <purplemrb12@gmail.com>
2025-02-14 19:37:16 -0500

    Basic door-switch implementation

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class DoorController : MonoBehaviour {
+    public bool m_open = false;
+
+    SwitchController[] m_switchCons;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() {
+        GameObject[] switches = GameObject.FindGameObjectsWithTag("Switch");
+        m_switchCons = new SwitchController[switches.Length];
+        for (int i = 0; i < switches.Length; i++) {
+            m_switchCons[i] = switches[i].GetComponent<SwitchController>();
+        }
+    }
+
+    // Update is called once per frame
+    // void Update() {
+        
+    // }
+
+    // open the door if all the switches are active; called whenever a switch is flipped
+    public void CheckSwitches() {
+        foreach (SwitchController switchCon in m_switchCons) {
+            if (switchCon.m_active == false) return;
+        }
+        m_open = true;
+    }
+}
---
+++ Assets/Scripts/SwitchController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class SwitchController : MonoBehaviour {
+    public bool m_active = false;
+
+    DoorController m_doorCon;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() {
+        m_doorCon = GameObject.Find("ExitDoor").GetComponent<DoorController>();
+    }
+
+    // Update is called once per frame
+    // void Update() {
+        
+    // }
+
+    void Activate() {
+        m_active = true;
+        m_doorCon.CheckSwitches();
+    }
+
+    // DEBUG PURPOSES ONLY
+    void OnMouseDown() {
+        Activate();
+    }
+}
